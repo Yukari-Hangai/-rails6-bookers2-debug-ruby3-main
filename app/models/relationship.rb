3afbb3f1b60cb1bf02bd6e9b62cedc96415551a1
Yukari Hangai <129511468+Yukari-Hangai@users.noreply.github.com>
2023-05-06 10:00:45 +0000

    応用課題4復習　アソシエーションまで

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,2 +1,9 @@
 class Relationship < ApplicationRecord
+  
+  belongs_to :follower, class_name: "User"#フォローしたユーザー
+  belongs_to :followed, class_name: "USer"#フォローされたユーザー
+  #フォローしたユーザーとフォローされたユーザーは本来同じUserモデルから
+  #.  持ってきたいがbelongs_to UserとするとどちらのUserかわからないので分けている
+  #.  ただ、それだけだとfollowerテーブルとfollowedテーブルを探しに行ってしまうので
+  #.  class: "User"でUserテーブルからデータをとってくるように指示している
 end
